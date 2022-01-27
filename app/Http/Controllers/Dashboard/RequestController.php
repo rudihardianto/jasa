82d6248e3297f1e1037453ada8a69c202d1e8ea0
Rudi Hardianto <77372397+rudihardianto@users.noreply.github.com>
2022-01-27 10:42:35 +0700

    request controller

--- app/Http/Controllers/Dashboard/RequestController.php
+++ app/Http/Controllers/Dashboard/RequestController.php
@@ -3,49 +3,44 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class RequestController extends Controller
 {
+   public function __construct()
+   {
+      $this->middleware('auth');
+   }
+
    public function index()
    {
-      return view('pages.dashboard.request.index');
-   }
+      $orders = Order::where('buyer_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
 
-   public function create()
-   {
-      //
-   }
-
-   public function store(Request $request)
-   {
-      //
+      return view('pages.dashboard.request.index', compact('orders'));
    }
 
    public function show($id)
    {
-      return view('pages.dashboard.request.detail');
-   }
+      //    detail
+      $order = Order::where('id', $id)->first();
 
-   public function edit($id)
-   {
-      //
-   }
-
-   public function update(Request $request, $id)
-   {
-      //
-   }
-
-   public function destroy($id)
-   {
-      //
+      return view('pages.dashboard.request.detail', compact('order'));
    }
 
    //    custom
    public function approve($id)
    {
-      ;
-   }
+      $order = Order::where('id', $id)->first();
 
+      //   update order
+      $order                  = Order::find($order['id']);
+      $order->order_status_id = 1;
+      $order->save();
+
+      Alert::success('Data berhasil di HAPUS');
+
+      return redirect()->route('member.request.index');
+   }
 }
