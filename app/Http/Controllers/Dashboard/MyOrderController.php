d7545b9ed2ac873a143ca891bad5b417479addf8
Rudi Hardianto <77372397+rudihardianto@users.noreply.github.com>
2022-01-27 11:01:44 +0700

    order controller

--- app/Http/Controllers/Dashboard/MyOrderController.php
+++ app/Http/Controllers/Dashboard/MyOrderController.php
@@ -3,54 +3,86 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Dashboard\Order\UpdateOrderRequest;
+use App\Models\AdvantageService;
+use App\Models\AdvantageUser;
+use App\Models\Order;
+use App\Models\Service;
+use App\Models\Tagline;
+use App\Models\ThumbnailService;
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class MyOrderController extends Controller
 {
+   public function __construct()
+   {
+      $this->middleware('auth');
+   }
+
    public function index()
    {
-      return view('pages.dashboard.order.index');
+      $services = Order::where('users_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
+
+      return view('pages.dashboard.order.index', compact('orders'));
    }
 
-   public function create()
+   public function show(Order $order)
    {
-      //
+      $service           = Service::where('id', $order['service_id'])->first();
+      $service           = ThumbnailService::where('service_id', $order['service_id'])->get();
+      $advantage_service = AdvantageService::where('service_id', $order['service_id'])->get();
+      $advantage_user    = AdvantageUser::where('service_id', $order['service_id'])->get();
+      $tagline           = Tagline::where('service_id', $order['service_id'])->get();
+
+      return view('pages.dashboard.order.detail', compact('order', 'service', 'thumbnail', 'advantage_service', 'advantage_user', 'tagline'));
    }
 
-   public function store(Request $request)
+   public function edit(Order $order)
    {
-      //
+      return view('pages.dashboard.order.edit', compact('order'));
    }
 
-   public function show($id)
+   public function update(UpdateOrderRequest $request, Order $order)
    {
-      return view('pages.dashboard.order.detail');
-   }
+      $data = $request->all();
+      if (isset($data['file'])) {
+         $data['file'] = $request->file('file')->store(
+            'assets/order/attachment', 'public'
+         );
+      }
 
-   public function edit($id)
-   {
-      return view('pages.dashboard.order.edit');
-   }
+      $order       = Order::find($order->id);
+      $order->file = $data['file'];
+      $order->note = $data['note'];
+      $order->save();
 
-   public function update(Request $request, $id)
-   {
-      //
-   }
+      Alert::success('Data berhasil di UPDATE');
 
-   public function destroy($id)
-   {
-      //
+      return redirect()->route('member.order.index');
    }
 
    //    custom
    public function accepted($id)
    {
-      ;
+      $order                  = Order::find($id);
+      $order->order_status_id = 2;
+      $order->save();
+
+      Alert::success('konfirmasi order diterima');
+
+      return redirect()->route('member.order.index');
    }
 
    public function rejected($id)
    {
-      ;
+      $order                  = Order::find($id);
+      $order->order_status_id = 3;
+      $order->save();
+
+      Alert::success('tolak order diterima');
+
+      return redirect()->route('member.order.index');
    }
 
 }
